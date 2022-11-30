ef50c20db97a8b51e2ed9f6f73d979391c3df60b
asliddin2002developer <eshonkulovasliddindeveloper@gmail.com>
2022-11-30 19:56:59 +0500

    loop exercises.

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
+        int mid = sideLength / 2;
+        for (int i = 0; i < sideLength; i++){
+            for (int j = 0; j < sideLength; j++){
+                if (j == mid || i == mid){
+                    System.out.print("8");
+                }else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
 
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,47 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        boolean reverse = false;
+        int start = 0;
+        int end = height-1;
+        for (int i=0; i < height; i++){
+            if (end - start >= 0) {
+                for (int j = 0; j < height; j++) {
+                    if (j >= start && j <= end) {
+                        System.out.print("8");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                if (reverse) {
+                    start--;
+                    end++;
+                    System.out.println();
+                }else{
+                    start++;
+                    end--;
+                    System.out.println();
+                }
+            }else {
+                reverse = true;
+                if (height % 2 == 0){
+                    start--;
+                    end++;
+                }else{
+                    start -= 2;
+                    end += 2;
+                }
+                for (int j = 0; j < height; j++) {
+                    if (j >= start && j <= end) {
+                        System.out.print("8");
+                    } else {
+                        System.out.print(" ");
+                    }
+                }
+                start--;
+                end++;
+                System.out.println();
+            }
+        }
     }
 }
